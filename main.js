01f45d1a7653b80cc78ac1ca6371bd5726e22346
Charity Bunyon <cseymou5@gmail.com>
2019-08-28 19:57:33 -0500

    added a petSite function

--- main.js
+++ main.js
@@ -43,3 +43,26 @@ const pets = [
     type: 'dino'
 }
 ]
+
+
+//1a. Create an arrow function that will pass my array of pies
+
+const petSite = (petsArray) => {
+    let domString = ''; 
+    // 5a.This variable is going to allow print out the div I want below.
+    for (let i = 0; i < petsArray.length; i++) {
+    //2a. Created for loop that is going to loop through my pets array to retrieve objects
+    domString += `
+        <div class = "card">
+            <h2>${pets.name}</h2>
+            <img src = ${pets.img} alt ='Image of ${pets.name}'>
+            <p>${pets.color}</p>
+            <p>${pets.skill}</p>
+            <p>${pets.type}</p>
+        </div>
+    `
+    // 3a. This domstring is going to print these items to my page, but I'm telling the loop to create a div for each of the objects. Giving a class of card so that i can apply styles.
+    printToDom(domString, 'pet-zone');
+    // 4a.This printToDom function is going to call or pass through the following parameters. But I haven't made this function yet.
+    }
+}
